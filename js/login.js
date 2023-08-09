b837c7fd5942833699e182a19a518a80ab8b66cb
paulaa99 <paau.a@hotmail.com>
2023-08-08 21:01:14 -0300

    no mergear

--- js/login.js
+++ js/login.js
@@ -1,3 +1,18 @@
+function datos_requeridos(){
+      
+    usuario = document.getElementById("usuario").value;
+    contra  = document.getElementById("contra").value;
+    capcha  = document.getElementById("capcha").checked;
+
+  if (usuario == ''||contra == ''||capcha == false) {
+    alert('Usuario, contraseña y capcha son obligatorios');
+    return false;
+  }else if (usuario != ''&&contra != ''&&capcha){
+  window.location.replace('https://mathiasbernardico.github.io/workspace-inicial');
+  localStorage.setItem('isLoggedIn', 'true');
+  }
+}
+
 const formulario = document.getElementById('loginForm');
 
 loginForm.addEventListener('submit', function(event) {
@@ -5,3 +20,13 @@ loginForm.addEventListener('submit', function(event) {
 
     window.location.href = 'index.html';
 });
+
+
+function logout() {
+    localStorage.removeItem('isLoggedIn');
+  }
+  document.addEventListener('click', function(event) {
+    if (event.target.id === 'logout') {
+      logout();
+    }
+  });
