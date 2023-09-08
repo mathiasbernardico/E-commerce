e218f6f8c2cf426c4e56e93066f86f51948bfa80
ericks19 <ericknz2014@gmail.com>
2023-09-08 15:07:24 -0300

    Funcionamiento de mostrar comentarios terminado

--- js/product-info.js
+++ js/product-info.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const URLProduct = `https://japceibal.github.io/emercado-api/products/${idProducto}.json`;
   const URLComentarios = `https://japceibal.github.io/emercado-api/products_comments/${idProducto}.json`;
   const containerInfo = document.getElementById("container-info");
-  const divParaComentarios = document.getElementById("comentarios")
+  const divParaComentarios = document.getElementById("comentarios");
   // Funcion para renderizar la info seleccionada en pantalla
   function mostrarInfoProducto(obj) {
     containerInfo.innerHTML += `<h1>${obj.name} </h1>`;
@@ -27,47 +27,51 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.json())
     .then((data) => {
       mostrarInfoProducto(data);
+      fetch(URLComentarios) //Segundo fetch en el .then del primero para asegurarnos de que siempre haga las solicitudes en este orden
+        .then((response) => response.json())
+        .then((data) => {
+          mostrarComentarios(data);
+        });
     })
     //Retornamos el error por consola
     .catch((error) => {
       console.error("Error:", error);
     });
 
-//Empieza codigo para renderizado de comentarios
+  //Empieza codigo para renderizado de comentarios
   const estrellas = [
     '<span class="fa fa-star"></span>',
     '<span class="fa fa-star"></span>',
     '<span class="fa fa-star"></span>',
     '<span class="fa fa-star"></span>',
-    '<span class="fa fa-star"></span>'
+    '<span class="fa fa-star"></span>',
   ];
-  function rellenarEstrellas(estrellas, calificacion){
-    for (let i = 0; i <= estrellas.length; i++){
-      if(i < calificacion){
-        estrellas[i] = '<span class="fa fa-star checked"></span>'
+  function rellenarEstrellas(estrellas, calificacion) {
+    for (let i = 0; i <= estrellas.length; i++) {
+      if (i < calificacion) {
+        estrellas[i] = '<span class="fa fa-star checked"></span>';
       }
     }
-    return estrellas
+    estrellas = estrellas.join(""); //usamos el metodo join para convertir el array en una sola cadena de texto
+    return estrellas;
   }
   function mostrarComentarios(lista) {
     const divContenedorComentarios = document.createElement("div");
     divContenedorComentarios.classList.add("contenedor-comentarios");
+    const tituloComentarios = document.createElement("h2");
+    tituloComentarios.innerText = "Comentarios";
+    divContenedorComentarios.appendChild(tituloComentarios);
     for (let comentario of lista) {
       const divComentario = document.createElement("div");
       divComentario.classList.add("div-comentario");
       const usuario = comentario.user;
       const fecha = comentario.dateTime;
       const description = comentario.description;
-      const calificacion = rellenarEstrellas(estrellas,comentario.score);
-      divComentario.innerHTML += `<p>${usuario} ${fecha} ${calificacion}</p>`
-      divComentario.innerHTML += `<p>${description}</p>`
-      divContenedorComentarios.appendChild(divComentario)
+      const calificacion = rellenarEstrellas(estrellas, comentario.score);
+      divComentario.innerHTML += `<p>${usuario} - ${fecha} - ${calificacion}</p>`;
+      divComentario.innerHTML += `<p>${description}</p>`;
+      divContenedorComentarios.appendChild(divComentario);
     }
-    containerInfo.appendChild(divContenedorComentarios)
+    containerInfo.appendChild(divContenedorComentarios);
   }
-  fetch(URLComentarios)
-    .then((response) => response.json())
-    .then((data) => {
-      mostrarComentarios(data);
-    });
 });
--- js/products.js
+++ js/products.js
@@ -1,4 +1,3 @@
-
 //Variables locales
 let item = localStorage.getItem("catID");
 const URLProducts = `https://japceibal.github.io/emercado-api/cats_products/${item}.json`;
@@ -7,8 +6,13 @@ const busqueda = document.getElementById("busqueda"); //Se genera constante busq
 
 function productos(listaDeProductos, textobuscado) {
   productList.innerHTML = ""; // Vacía el contenido de productList para volver a generarlo
-  for (let producto of listaDeProductos) { //Recorremos la listaDeProductos buscando en cada producto
-    if (producto.name.toLowerCase().includes(textobuscado) || producto.description.toLowerCase().includes(textobuscado)){ //Se evalua que el nombre o la descripcion incluya el texto buscado (el texto lo comparamos transformandolo todo a minuscula)
+  for (let producto of listaDeProductos) {
+    //Recorremos la listaDeProductos buscando en cada producto
+    if (
+      producto.name.toLowerCase().includes(textobuscado) ||
+      producto.description.toLowerCase().includes(textobuscado)
+    ) {
+      //Se evalua que el nombre o la descripcion incluya el texto buscado (el texto lo comparamos transformandolo todo a minuscula)
       //Generamos nuestra estructura html
       let divProductContainer = document.createElement("div");
       divProductContainer.classList.add("car-card");
@@ -18,39 +22,44 @@ function productos(listaDeProductos, textobuscado) {
       <span class="product-value"> ${producto.soldCount} vendidos </span> </p>`;
       divProductContainer.innerHTML += `<p> ${producto.description} </p>`;
 
-      divProductContainer.addEventListener("click", () => { //Agregamos el evento click en el div anteriormente creado
-        localStorage.setItem('id_producto', producto.id);
-        window.location.href = 'product-info.html';
+      divProductContainer.addEventListener("click", () => {
+        //Agregamos el evento click en el div anteriormente creado
+        localStorage.setItem("id_producto", producto.id);
+        window.location.href = "product-info.html";
       });
     }
-    
   }
 }
 
-function clasificacion_products(dato) { //Creamos la funcion para clasificar el producto en funcion del dato
+function clasificacion_products(dato) {
+  //Creamos la funcion para clasificar el producto en funcion del dato
   fetch(URLProducts)
     .then((response) => response.json())
-    .then((data) => { 
-      categoria(data.catName); //Llamamos a la funcion categoria para colocar en el parrafo el nombre de la categoria 
+    .then((data) => {
+      categoria(data.catName); //Llamamos a la funcion categoria para colocar en el parrafo el nombre de la categoria
 
       if (dato == "0") {
-        data.products.sort(function (a, b) { //Clasificacion acendente (Precio)
+        data.products.sort(function (a, b) {
+          //Clasificacion acendente (Precio)
           return a.cost - b.cost;
         });
       } else if (dato == "1") {
-        data.products.sort(function (a, b) { //Clasificacion decendente (Precio)
+        data.products.sort(function (a, b) {
+          //Clasificacion decendente (Precio)
           return b.cost - a.cost;
         });
       } else if (dato == "2") {
-        data.products.sort(function (a, b) { //Clasificacion decendente (Relevancia)
+        data.products.sort(function (a, b) {
+          //Clasificacion decendente (Relevancia)
           return b.soldCount - a.soldCount;
         });
       }
       productos(data.products, "");
-  });
+    });
 }
 
-function categoria(categoria) { //Colocamos en el parrafo el nombre de la categoria 
+function categoria(categoria) {
+  //Colocamos en el parrafo el nombre de la categoria
   let categoryParagraph = document.getElementById("category-paragraph");
   categoryParagraph.innerHTML = `Verás aquí todos los productos de la categoría ${categoria} `;
 }
@@ -62,14 +71,15 @@ fetch(URLProducts)
     productos(data.products, "");
   }); //Se incluyen todos los productos
 
-busqueda.addEventListener("input", function () { //Si se utiliza el input de busqueda
+busqueda.addEventListener("input", function () {
+  //Si se utiliza el input de busqueda
   const textobuscado = busqueda.value.toLowerCase(); //Transformamos a minuscula
   fetch(URLProducts)
     .then((response) => response.json())
     .then((data) => {
       categoria(data.catName);
       productos(data.products, textobuscado); //Llama a productos con el termino de busqueda nuevo
-  });
+    });
 });
 
 //Variables locales
@@ -78,22 +88,28 @@ const inputMaximo = document.getElementById("input-maximo");
 const botonFiltrarRangos = document.getElementById("boton-filtrar-por-rangos");
 
 function filtrarMinMax(lista) {
-  if(inputMaximo.value !== "" && inputMinimo.value !== ""){ //En el caso que rellene los dos campos, controlamos que los dos no esten vacios
-    return lista.filter((item) => { //Utilizamos .filter para filtrar y utilizamos una funcion flecha para aplicar la siguiente condicion
+  if (inputMaximo.value !== "" && inputMinimo.value !== "") {
+    //En el caso que rellene los dos campos, controlamos que los dos no esten vacios
+    return lista.filter((item) => {
+      //Utilizamos .filter para filtrar y utilizamos una funcion flecha para aplicar la siguiente condicion
       return item.cost >= inputMinimo.value && item.cost <= inputMaximo.value;
     });
-  }else if(inputMaximo.value !== ""){ //En caso que solo rellene el inputMaximo
+  } else if (inputMaximo.value !== "") {
+    //En caso que solo rellene el inputMaximo
     return lista.filter((item) => {
       return item.cost <= inputMaximo.value;
     });
-  }else if(inputMinimo.value !== ""){ //En caso que solo rellene el inputMinimo
+  } else if (inputMinimo.value !== "") {
+    //En caso que solo rellene el inputMinimo
     return lista.filter((item) => {
       return item.cost >= inputMinimo.value;
     });
   }
-  }
-  botonFiltrarRangos.addEventListener("click", () => { //Evento click para el boton filtrar
-  if (inputMaximo.value !== "" || inputMinimo.value !== "") { //Controlamos que no esten vacios
+}
+botonFiltrarRangos.addEventListener("click", () => {
+  //Evento click para el boton filtrar
+  if (inputMaximo.value !== "" || inputMinimo.value !== "") {
+    //Controlamos que no esten vacios
     fetch(URLProducts)
       .then((response) => response.json())
       .then((data) => {
