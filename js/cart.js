d3f397a67bf496e36bced03f9eb3dd4ea8c076ea
ericks19 <ericknz2014@gmail.com>
2023-10-10 04:05:53 -0300

    Punto numero dos entrega 5 terminado

--- js/cart.js
+++ js/cart.js
@@ -3,44 +3,33 @@
 document.addEventListener("DOMContentLoaded", function () {
     const IDinicial =25801;
     const url = `https://japceibal.github.io/emercado-api/user_cart/${IDinicial}.json`;
-  
 
     function mostrarDetallesCarrito(data) {
         const articles = data.articles;
-      
         const container = document.getElementById('detallesCarrito');
-      
         articles.forEach(article => {
           const articleDiv = document.createElement('div');
           articleDiv.classList.add('article');
-      
-
           const name = document.createElement('h2');
           name.innerHTML = article.name;
           articleDiv.appendChild(name);
-       
           const quantity = document.createElement('p');
           quantity.innerHTML = `Cantidad: ${article.count}`;
           articleDiv.appendChild(quantity);
-      
           const cost = document.createElement('p');
           cost.innerHTML = `Precio unitario: ${article.unitCost} ${article.currency}`;
           articleDiv.appendChild(cost);
-      
           const image = document.createElement('img');
           image.src = article.image;
           image.alt = article.name;
           articleDiv.appendChild(image);
-      
           const subtotal = document.createElement('p');
           const monto = article.unitCost * article.count;
           subtotal.innerHTML = `Subtotal: ${monto} ${article.currency}`;
           articleDiv.appendChild(subtotal);
-      
           container.appendChild(articleDiv);
         });
       }
-  
     fetch(url)
       .then(response => response.json())
       .then(data => {
@@ -50,4 +39,3 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error('Error', error);
       });
   });
-  
