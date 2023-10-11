7d8ca12b4caad382ca7753d4b91b06accbc2f50d
paulaa99 <paau.a@hotmail.com>
2023-10-11 19:27:15 -0300

    Carrito
    Se cambia el carrito para añadirlo a una tabla con headers.
    Se ajusta css para tomar en cuenta la tabla y se añade diseño responsivo

--- js/cart.js
+++ js/cart.js
@@ -1,51 +1,81 @@
 // cart.js
 
 document.addEventListener("DOMContentLoaded", function () {
-  const IDinicial = 25801;
-  const url = `https://japceibal.github.io/emercado-api/user_cart/${IDinicial}.json`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      mostrarDetallesCarrito(data);
-    })
-    .catch((error) => {
-      console.error("Error", error);
-    });
+    const IDinicial =25801;
+    const url = `https://japceibal.github.io/emercado-api/user_cart/${IDinicial}.json`;
+  
 
-  function mostrarDetallesCarrito(data) {
-    const articles = data.articles;
-    const container = document.getElementById("detallesCarrito");
-    articles.forEach((article) => {
-      const articleDiv = document.createElement("div");
-      articleDiv.classList.add("article");
-      const name = document.createElement("h2");
-      name.innerHTML = article.name;
-      articleDiv.appendChild(name);
-      const cost = document.createElement("p");
-      cost.innerHTML = `Precio unitario: ${article.unitCost} ${article.currency}`;
-      articleDiv.appendChild(cost);
-      const quantity = document.createElement("p");
-      quantity.innerHTML = `Cantidad:`;
-      articleDiv.appendChild(quantity);
-      const inputQuantity = document.createElement("input");
-      inputQuantity.type = "number";
-      inputQuantity.name = "inputQuantity";
-      inputQuantity.id = "inputQuantity";
-      inputQuantity.value = `${article.count}`
-      articleDiv.appendChild(inputQuantity)
-      inputQuantity.addEventListener("input", function () {
-        const newCost = article.unitCost * inputQuantity.value;
-        subtotal.innerHTML = `Subtotal: ${newCost} ${article.currency}`;
+    function mostrarDetallesCarrito(data) {
+      const articles = data.articles;
+      const container = document.getElementById("detallesCarrito");
+      const table = document.createElement("table");
+      table.classList.add("table");
+      const thead = document.createElement("thead");
+      const headerRow = document.createElement("tr");
+      const columnHeaders = ["", "Nombre", "Precio Unitario", "Cantidad", "Subtotal"];
+    
+      columnHeaders.forEach((headerText) => {
+        const th = document.createElement("th");
+        th.textContent = headerText;
+        headerRow.appendChild(th);
       });
-      const subtotal = document.createElement("p");
-      const monto = article.unitCost * inputQuantity.value;
-      subtotal.innerHTML = `Subtotal: ${monto} ${article.currency}`;
-      articleDiv.appendChild(subtotal);
-      const image = document.createElement("img");
-      image.src = article.image;
-      image.alt = article.name;
-      articleDiv.appendChild(image);
-      container.appendChild(articleDiv);
-    });
-  }
-});
+    
+      thead.appendChild(headerRow);
+      table.appendChild(thead);
+  
+      const tbody = document.createElement("tbody");
+      articles.forEach((article) => {
+        const row = document.createElement("tr");
+    
+       
+        const imageCell = document.createElement("td");
+        const image = document.createElement("img");
+        image.src = article.image;
+        image.alt = article.name;
+        imageCell.appendChild(image);
+        row.appendChild(imageCell);
+    
+        
+        const nombreCell = document.createElement("td");
+        nombreCell.textContent = article.name;
+        row.appendChild(nombreCell);
+    
+        const precioUnitarioCell = document.createElement("td");
+        precioUnitarioCell.textContent = `${article.unitCost} ${article.currency}`;
+        row.appendChild(precioUnitarioCell);
+    
+        const cantidadCell = document.createElement("td");
+        const inputQuantity = document.createElement("input");
+        inputQuantity.type = "number";
+        inputQuantity.name = "inputQuantity";
+        inputQuantity.id = "inputQuantity";
+        inputQuantity.value = article.count;
+        inputQuantity.addEventListener("input", function () {
+          const newCost = article.unitCost * inputQuantity.value;
+          subtotalCell.textContent = `Subtotal: ${newCost} ${article.currency}`;
+        });
+        cantidadCell.appendChild(inputQuantity);
+        row.appendChild(cantidadCell);
+    
+        const subtotalCell = document.createElement("td");
+        const monto = article.unitCost * article.count;
+        subtotalCell.textContent = `Subtotal: ${monto} ${article.currency}`;
+        row.appendChild(subtotalCell);
+    
+        tbody.appendChild(row);
+      });
+    
+      table.appendChild(tbody);
+      container.appendChild(table);
+    }
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        mostrarDetallesCarrito(data);
+      })
+      .catch(error => {
+        console.error('Error', error);
+      });
+  });
+  
